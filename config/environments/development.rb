7340bb0be68b00d39798883d920da104fe6a2a2d
nehemiahnewell <nehemiah.newell@gmail.com>
2016-05-04 16:46:35 +0000

    non-working mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'bloc-foundation-nehemiahnewell.c9users.io' }
+  config.action_mailer.default_url_options = { host: 'https://<workspacename>-<username>.c9users.io', port: 8081 }
 end
