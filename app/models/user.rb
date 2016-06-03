8d3565d8d410dd6b06b7ce3ab6110566652b2769
nehemiahnewell <nehemiah.newell@gmail.com>
2016-06-03 15:28:56 +0000

    lets see if this works

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :send_user_emails
+ 
+   private
+ 
+   def send_user_emails
+      LogonMailer.new_signup(self).deliver_now
+   end
 end
