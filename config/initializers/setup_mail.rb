3b596738c0286a43d8570e02b46be9db731a813d
Conrad Taylor <conradwt@gmail.com>
2016-06-28 01:52:56 -0700

    Add e-mail interceptor.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,19 @@
-if Rails.env.development? || Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address:        'smtp.sendgrid.net',
-    port:           '2525',
-    authentication: :plain,
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
-    domain:         'heroku.com',
-    enable_starttls_auto: true
-  }
-end
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address:             ENV['SMTP_ADDRESS'],
+  port:                ENV['SMTP_PORT'],
+  authentication:      :plain,
+  user_name:           ENV['SMTP_USER_NAME'],
+  password:            ENV['SMTP_PASSWORD'],
+  domain:              ENV['SMTP_DOMAIN'],
+  enable_starttls_auto: true
+}
+
+# address:             'smtp.sendgrid.net',
+# port:                '2525',
+# authentication:      :plain,
+# user_name:           ENV['SENDGRID_USERNAME'],
+# password:            ENV['SENDGRID_PASSWORD'],
+# domain:              'heroku.com',
+# enable_starttls_auto: true
+# }
