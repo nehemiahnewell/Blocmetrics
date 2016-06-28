90251571b4cb1a9aa65a1bfe08e4541572c24205
Conrad Taylor <conradwt@gmail.com>
2016-06-27 18:13:22 -0700

    Add confirmable module and do a bit of code cleanup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
+
   after_create :send_user_emails
   has_many :registered_applications
-   private
- 
-   def send_user_emails
-      LogonMailer.new_signup(self).deliver_now
-   end
+
+  private
+
+  def send_user_emails
+    LogonMailer.new_signup(self).deliver_now
+  end
 end
