1c4cd62e0c689fedd906ed65fd069d38491609ce
Conrad Taylor <conradwt@gmail.com>
2016-07-04 03:32:05 -0700

    Add a sign in controller and sample view template which requires authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+      if resource.is_a?(User)
+        registered_applications_path
+      else
+        super
+      end
+  end
 end
---
+++ app/controllers/registered_applications_controller.rb
@@ -0,0 +1,4 @@
+class RegisteredApplicationsController < ApplicationController
+  def index
+  end
+end
